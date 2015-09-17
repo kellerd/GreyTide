1f73df4798960c58b3486f1d4e5fe447d9964907
kellerd <danny.keller@tc.gc.ca>
2015-09-16 20:07:06 -0400

    Install hot towel. Get up and running demo

---
+++ Models/FromState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreyTideDataService.Models
+{
+    public class FromState
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid StateId { get { return State.Id; } set { return; } }
+        protected State State { get; set; }
+
+        public void SetState(State s)
+        {
+            State = s;
+        }
+    }
+}
---
+++ Models/StateCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace GreyTideDataService.Models
+{
+    //"name": "Default", "events": []
+    public class StateCollection
+    {
+        public Guid UserToken { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set ; }
+        public List<State> Events {get;set;} 
+    }
+}
