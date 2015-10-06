d69d9e54e704c5002f215eeabe2915e82f03cbc7
kellerd <danny.keller@tc.gc.ca>
2015-10-05 22:56:05 -0400

    Tide connection string

---
+++ GreyTide/data/AzureConStr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreyTide.data
+{
+    public class HardDriveAzureConnectionStr
+    {
+        private static Lazy<string> _ConnectionKey = new Lazy<string>(() => System.IO.File.ReadAllText("C:\azdocdb.txt"), true);
+        public static string ConnectionKey
+        {
+            get {
+                return _ConnectionKey.Value;
+            }
+        }
+
+        private static Lazy<Uri> _ConnectionUri = new Lazy<Uri>(() => new Uri(System.IO.File.ReadAllText("C:\azdocdburi.txt")), true);
+        public static Uri ConnectionUri
+        {
+            get
+            {
+                return _ConnectionUri.Value;
+            }
+        }
+    }
+}
